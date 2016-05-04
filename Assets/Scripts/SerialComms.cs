4f1525163fca39ed24383cca477e5d67811a112a
Keir Meikle <keir.meikle@gmail.com>
2016-05-04 22:33:41 +1000

    Basic unity project with test scenes and physics/modbus setup.

---
+++ Assets/Scripts/SerialComms.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using Modbus;
+using System.IO.Ports;
+
+public class SerialComms : MonoBehaviour {
+
+	protected SerialPort m_serial;
+
+	void Start()
+	{
+		Init ();
+	}
+
+	protected virtual void Init () 
+	{
+		m_serial = new SerialPort("/dev/tty.usbserial-A101OCIF",9600);
+		if (m_serial != null)
+			m_serial.Open();
+		Debug.Log("Opened serial port.");
+		//SerialWrite(" ");
+	}
+	
+	public virtual void Stop () 
+	{
+		SerialWrite("s\r");
+	}
+
+	public virtual void Move(int speed) 
+	{
+		string command = string.Empty;
+		command += (speed < 0) ? "r" : "f";		//forwards or backwards
+		command += Mathf.Abs(speed).ToString();	//send speed value 0-255
+		command += "\r";						//return
+
+		SerialWrite(command);
+	}
+
+	protected virtual void SerialWrite(string command)
+	{
+		Debug.Log ("Sending command: " + command);
+		if (m_serial != null && m_serial.IsOpen)
+		{ 
+			m_serial.Write(command);
+		}
+		else
+		{
+			Debug.Log("Command not set. Serial port is not open.");
+		}
+	}
+}
