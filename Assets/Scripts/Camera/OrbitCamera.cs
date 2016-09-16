4a49984ae0e9f321acae5521cfbbba823ff34cc5
keir.meikle <keir.meikle@rea-group.com>
2016-09-16 12:01:21 +0800

    Added very roughly placed hind leg to Vehicle scene.

---
+++ Assets/Scripts/Camera/OrbitCamera.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class OrbitCamera : MonoBehaviour
+{
+	public float m_orbitSpeed = 1.0f;
+	public float m_zoomSpeed = 1.0f;
+	public float m_minZoom = 0.5f;
+	public float m_maxZoom = 20.0f;
+
+	void Start()
+	{
+
+	}
+
+	void Update()
+	{
+
+
+	}
+}
